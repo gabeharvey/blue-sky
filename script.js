beac48667d46022cfdb5c8358ad911eaa5e78999
Gabe Harvey <harvey7@gmail.com>
2024-06-25 18:43:14 -0500

    Added addl script.js statement

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 console.log('This is our family');
+
 let memberOne = 'Gabe';
 console.log(memberOne + ' is in our family');
+
+const hobbyOne = "Playing World of Warcraft";
+console.log('Hello my name is ' + memberOne + ' and I like ' + hobbyOne);
