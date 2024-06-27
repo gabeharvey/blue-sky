16e5af25ede4c3366f4238fe63504e3c8cd14029
Gabe Harvey <harvey7@gmail.com>
2024-06-26 21:27:42 -0500

    Added primitive Javascript

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ console.log(memberOne + ' is in our family');
 
 const hobbyOne = "Playing World of Warcraft";
 console.log('Hello my name is ' + memberOne + ' and I like ' + hobbyOne);
+
+let one = 42;
+let two = false;
+
+console.log(typeof one);
+console.log(typeof two);
