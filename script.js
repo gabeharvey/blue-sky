0511335d295304eea32aa3f99e3f798bfb9eacde
Gabe Harvey <harvey7@gmail.com>
2024-06-29 11:39:34 -0500

    For Loop added to script.js

--- script.js
+++ script.js
@@ -25,3 +25,8 @@ console.log(family[1]);
 console.log(family[2]);
 console.log(family[3]);
 console.log(family[4]);
+
+console.log(family.length);
+for (let i=0; i<family.length; i++) {
+    console.log(family[i] + ' is in the Harvey Family');
+}
